09050a14cfb236a8d89054b49e0db7b5a81d1a84
aishrahatal <aishwarya.rahatal97@gmail.com>
2021-11-18 19:20:29 +0530

    [Aishwarya] Add: Calculating euclidean distance

---
+++ FunctionalPrograms/Distance.py
@@ -0,0 +1,30 @@
+'''	
+@Author: Aishwarya
+@Date: 2021-11-18 
+@Title : Write program for Euclidean distance from the point (x, y) to the origin (0, 0)
+'''
+########################################################################################################
+#importing Regex liabrary 
+import re
+import math
+if __name__=='__main__':
+   # Taking input from the user
+    print("Enter the first point A")
+    x = input("Enter value of x coordinate =")
+    y = input("Enter value of x coordinate =")
+    print("The Origin point O(0,0) ")
+    #regex for integer number and maximum two digit 
+    pattern= "^[0-9]|-[0-9]{1,2}$"
+    result1=re.match(pattern,x)
+    result2=re.match(pattern,y)
+    if result1 and result2:
+        x=int(x)
+        y=int(y)
+        #calculating distance from origin point O(0,0) an A(x,y)
+        distance = math.sqrt(pow(x,2) + pow(y,2))
+        #used trunc() for trimming decimal
+        print("Euclidean distance of given points O(0,0) an A(x,y) =", math.trunc(distance))
+
+    else:
+        print("Invalid Input")
+    
