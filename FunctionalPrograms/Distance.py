2c5e30118ef6816737f85efbaced165dee632423
aishrahatal <aishwarya.rahatal97@gmail.com>
2021-11-19 12:23:07 +0530

    [Aishwarya] Refactor: created function for calculating distance

--- FunctionalPrograms/Distance.py
+++ FunctionalPrograms/Distance.py
@@ -1,14 +1,23 @@
 '''	
 @Author: Aishwarya
 @Date: 2021-11-18 
+@Last: Modified: 2021-11-19
 @Title : Write program for Euclidean distance from the point (x, y) to the origin (0, 0)
 '''
 ########################################################################################################
 #importing Regex liabrary 
 import re
 import math
+
+def get_distance(x,y):
+   #calculating distance from origin point O(0,0) an A(x,y)
+    distance = math.sqrt(pow(x,2) + pow(y,2))
+    #used trunc() for trimming decimal after decimal point
+    print("Euclidean distance of given points O(0,0) an A(x,y) =", math.trunc(distance))
+
+
 if __name__=='__main__':
-   # Taking input from the user
+# Taking input from the user
     print("Enter the first point A")
     x = input("Enter value of x coordinate =")
     y = input("Enter value of x coordinate =")
@@ -20,11 +29,7 @@ if __name__=='__main__':
     if result1 and result2:
         x=int(x)
         y=int(y)
-        #calculating distance from origin point O(0,0) an A(x,y)
-        distance = math.sqrt(pow(x,2) + pow(y,2))
-        #used trunc() for trimming decimal
-        print("Euclidean distance of given points O(0,0) an A(x,y) =", math.trunc(distance))
-
+        get_distance(x,y)  
     else:
         print("Invalid Input")
     
