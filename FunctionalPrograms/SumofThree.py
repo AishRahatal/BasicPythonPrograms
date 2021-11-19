294f3cbddbcecc590855f9f26cfcd396a1b8f685
aishrahatal <aishwarya.rahatal97@gmail.com>
2021-11-19 12:29:25 +0530

    [Aishwarya] Refactor: changed function name and add functionality of getting multiple combination of three numbers which are in array

--- FunctionalPrograms/SumofThree.py
+++ FunctionalPrograms/SumofThree.py
@@ -1,6 +1,7 @@
 '''	
 @Author: Aishwarya
 @Date: 2021-11-18 
+@Last: Modified: 2021-11-19
 @Title : Sum of three Integer adds to ZERO
 '''
 ########################################################################################################
@@ -8,27 +9,29 @@
 from enum import Flag
 import re
 #function for trplet
-def FindTriplets(arr, n):
+def find_triplets(arr, n):
     """"
     Description: Brute Force approach to find distinct integer triplets which adds to zero
     Parameter: arr,n
     Return: none
     """
+    Flag=False
+
     for i in range(0 , n-2):
       for j in range(i+1,n-1):
         for k in range(j + 1, n):
           if arr[i] + arr[j] +arr[k] == 0:
+              print("There are Triplets !!")
+              print(arr[i], "  ", arr[j] ," ",arr[k] , sep = " ") 
+              print("------------------------------------")
               Flag=True
           else:
-              Flag=False
-    if Flag:
-        print("There are Triplets !!")
-        print(arr[i], "  ", arr[j] ," ",arr[k] , sep = " ") 
-    else:
-        print("Tripletes not present in array")             
-        print()
-
-
+               Flag=False
+               break
+              
+    if False:
+        print("Tripletes not present in array")      
+   
 if __name__=='__main__':
    # Taking input from the user
     n=input("Enter number of elements in array :")
@@ -51,5 +54,5 @@ if __name__=='__main__':
          print(i)
           
         print("-----------------------")  
-        FindTriplets(a,n)
+        find_triplets(a,n)
     else:print("Invalid Input")
